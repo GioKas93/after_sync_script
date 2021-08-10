0b78869fba421402b8603e9b53a43ac2bd452f62
Gheorghe <giorgio_castellucci@libero.it>
2021-08-10 13:17:52 +0000

    I'm a lazy ass guy

---
+++ lazyass.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#Me a lazy fat ass guy,so me make this script to clone everything for building(DT,KRENUL etc..)
+
+cd nosfet
+
+git clone https://github.com/GioKas93/android_device_xiaomi_raphael -b teeeeest device/xiaomi/raphael
+
+git clone https://github.com/GioKas93/kernel_xiaomi_raphael -b imm-afammok kernel/xiaomi/raphael
+
+git clone https://github.com/GioKas93/android_device_xiaomi_raphael-sepolicy -b 11 device/xiaomi/raphael-sepolicy
+
+git clone https://github.com/UtsavBalar1231/device_xiaomi_raphael-kernel -b Rika device/xiaomi/raphael-kernel
+
+git clone https://github.com/GioKas93/vendor_xiaomi_raphael-2 -b 11 vendor/xiaomi/raphael
+
